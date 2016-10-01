fc031bf2fe91e815f39be336b15c5d5184e045f7
Haroon Ahmed <hrahmed@gmail.com>
2016-10-01 16:59:48 -0400

    Initial Commit

---
+++ MathApp-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathApp-IOS
+//
+//  Created by ahmha02 on 2016-10-01.
+//  Copyright © 2016 Haroon Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
