922c1418a3caea890b33cc84212b66096e444f57
Haroon Ahmed <hrahmed@gmail.com>
2016-10-20 20:44:04 -0400

    add ca maa sdk

--- MathApp-IOS/AppDelegate.swift
+++ MathApp-IOS/AppDelegate.swift
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        //Initialize CA AXA SDK
+            CAMDOReporter.initializeSDK(options: .SDKLogLevelVerbose) { (YES, nil) in print("CASDKisInitialized")
+                
+            }
+        
         // Override point for customization after application launch.
         return true
     }
---
+++ MathApp-IOS/MathApp-IOS-Bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  MathApp-IOS-Bridging-header.h
+//  MathApp-IOS
+//
+//  Created by ahmha02 on 2016-10-20.
+//  Copyright © 2016 Haroon Ahmed. All rights reserved.
+//
+
+#ifndef MathApp_IOS_Bridging_header_h
+#define MathApp_IOS_Bridging_header_h
+
+#import "CAMDOReporter.h"
+
+#endif /* MathApp_IOS_Bridging_header_h */
