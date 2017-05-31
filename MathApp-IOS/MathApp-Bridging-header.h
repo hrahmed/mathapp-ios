b4781f12f91a05b0d137068fc77382189ee2ce66
Haroon Ahmed <hrahmed@gmail.com>
2017-05-31 11:04:04 -0400

    new branch working the APM saas

---
+++ MathApp-IOS/MathApp-Bridging-header.h
@@ -0,0 +1,15 @@
+//
+//  MathApp-Bridging-header.h
+//  MathApp-IOS
+//
+//  Created by Ahmed, Haroon on 2017-05-31.
+//  Copyright © 2017 Haroon Ahmed. All rights reserved.
+//
+
+#ifndef MathApp_Bridging_header_h
+#define MathApp_Bridging_header_h
+
+#import "CAMDOReporter.h"
+
+
+#endif /* MathApp_Bridging_header_h */
